13e94fc2701068e2e347e33411b170979d406f82
João Hypolito <joaohypolito1@gmail.com>
2026-04-21 20:14:59 -0300

    feat: classes inicias do RPG

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace O_Ritual_de_bakbattahl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Guerreiro hurin = new Guerreiro("Hurin", 100);
+            Mago galadriel = new Mago("galadriel", 90);
+
+            hurin.RealizarAcao(galadriel);
+            galadriel.RealizarAcao(hurin);
+            
+        }
+    }
+}
